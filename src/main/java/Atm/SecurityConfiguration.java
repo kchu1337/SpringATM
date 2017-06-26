6912ff328688c5d3459087b338bf4d36b9935baa
Kevin Chu <kevinchu1337@gmail.com>
2017-06-26 15:17:34 -0400

    Added Validation

--- src/main/java/Atm/SecurityConfiguration.java
+++ src/main/java/Atm/SecurityConfiguration.java
@@ -19,8 +19,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-		.authorizeRequests().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers("/assets/**", "/bootstrap3/**").permitAll()
+		.anyRequest().authenticated();
 		http
 		.formLogin().failureUrl("/login?error")
 		.defaultSuccessUrl("/")
